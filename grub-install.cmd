ab7e6e33238e70bd1c546a5dd8bac0276a1e1edc
Feldwor <BoQsc@users.noreply.github.com>
2018-03-10 20:11:02 +0200

    Change hashtags to batch comments

--- grub-install.cmd
+++ grub-install.cmd
@@ -1,22 +1,22 @@
 @echo off
 
-#Change Current Directory to Where the grub-install.cmd is.
+::Change Current Directory to Where the grub-install.cmd is.
 CD "%~dp0"
 
-#Select install device for Grub
+::Select install device for Grub
 set "installDevice=\\?\G:"
 
-#On Windows we use UNC path. For volumes it’s typically
-# \\?\Volume{<GUID>}
-# \\?\<drive letter>:
-#E.g.
- # \\?\Volume{17f34d50-cf64-4b02-800e-51d79c3aa2ff}
- # \\?\C:
+::On Windows we use UNC path. For volumes it’s typically
+:: \\?\Volume{<GUID>}
+:: \\?\<drive letter>:
+::E.g.
+ :: \\?\Volume{17f34d50-cf64-4b02-800e-51d79c3aa2ff}
+ :: \\?\C:
 
-# For disks it’s
-# \\?\PhysicalDrive<number>
-#E.g. 
-# grub-install \\?\PhysicalDrive0
+:: For disks it’s
+:: \\?\PhysicalDrive<number>
+::E.g. 
+:: grub-install \\?\PhysicalDrive0
 
 
 grub-install.exe --boot-directory=G:\test --target=i386-pc %installDevice%
