280b955fa07eae32fdfdd67f12929ab4d2cd667f
Feldwor <BoQsc@users.noreply.github.com>
2018-03-10 20:01:10 +0200

    add comments about installpath
    Source: https://www.gnu.org/software/grub/manual/grub/grub.html#Installing-GRUB-using-grub_002dinstall

--- grub-install.cmd
+++ grub-install.cmd
@@ -6,5 +6,18 @@ CD "%~dp0"
 #Select install device for Grub
 set "installDevice=\\?\G:"
 
+#On Windows we use UNC path. For volumes it’s typically
+# \\?\Volume{<GUID>}
+# \\?\<drive letter>:
+#E.g.
+ # \\?\Volume{17f34d50-cf64-4b02-800e-51d79c3aa2ff}
+ # \\?\C:
+
+# For disks it’s
+# \\?\PhysicalDrive<number>
+#E.g. 
+# grub-install \\?\PhysicalDrive0
+
+
 grub-install.exe --boot-directory=G:\test --target=i386-pc %installDevice%
-pause
+pausea
